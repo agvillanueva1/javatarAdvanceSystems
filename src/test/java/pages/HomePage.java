ca1fdd0a3e40766f82dac9c898eef998953cf126
ketarin phophrom <kphophrom@gmail.com>
2022-12-10 10:54:11 -0500

    Deleted some code from HomePage.java and Finished ASJ-9

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -11,21 +11,4 @@ public class HomePage {
         PageFactory.initElements(BrowserUtils.getDriver(), this);
     }
 
-    @FindBy(xpath = "//li/a[text()='Home']")
-    public WebElement homeBtn;
-
-    @FindBy(xpath = "//a[text()='About Us']")
-    public WebElement aboutUsBtn;
-
-    @FindBy(xpath = "//a[text()='Services']")
-    public WebElement servicesBtn;
-
-    @FindBy(xpath = "//a[text()='Clients']")
-    public WebElement clientsBtn;
-
-    @FindBy(xpath = "//a[text()='Join Us']")
-    public WebElement joinUsBtn;
-
-    @FindBy(xpath = "//a[text()='Contact Us']")
-    public WebElement contactUsBtn;
 }
